08a08011becdd18c90f9b07dbea800a681212287
OceanApart <nevermind1984@outlook.com>
2017-10-05 15:30:51 +0800

    improve js code style && set current url when blank input

--- popup/show_qrcode.js
+++ popup/show_qrcode.js
@@ -1,52 +1,54 @@
 var qrcode = new QRCode("qrcode", {
     // text: "asdf",
-    width: 128,
-    height: 128,
+    width: 150,
+    height: 150,
     colorDark: "#000000",
     colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.H
 });
 
-function getActiveTab() {
-    return browser.tabs.query({currentWindow: true, active: true});
+function setInputText(url) {
+    document.getElementById("text").setAttribute("value", url);
 }
 
-/* old Firefox API */
-if (self && self.port) {
-    self.port.on('show', function onShow(data) {
-        qrcode.makeCode(data.url);
-    });
+function curURLToCode() {
+    /* old Firefox API */
+    if (self && self.port) {
+        self.port.on('show', function onShow(data) {
+            qrcode.makeCode(data.url);
+            setInputText(data.url);
+        });
+    }
+    /* WebExtensions API */
+    activeTab = browser.tabs.query({currentWindow: true, active: true});
+    if (browser.tabs && browser.tabs.query) {
+        activeTab.then(function (data) {
+            data = data[0];
+            /* only tab in set */
+            qrcode.makeCode(data.url);
+            setInputText(data.url);
+        });
+    }
 }
 
-/* WebExtensions API */
-if (browser.tabs && browser.tabs.query) {
-    getActiveTab().then(data => {
-        data = data[0];
-        /* only tab in set */
-        qrcode.makeCode(data.url);
-    });
-}
-
-function makeCode () {
+function makeCode() {
     var elText = document.getElementById("text");
 
-    // if (!elText.value) {
-    //     alert("Input a text");
-    //     elText.focus();
-    //     return;
-    // }
-
+    if (!elText.value) {
+        curURLToCode();
+        return;
+    }
     qrcode.makeCode(elText.value);
+
 }
 
+//  init
 makeCode();
-
-$("#text").
-on("blur", function () {
+//  listen on mouse event
+$("#text").on("blur", function () {
     // lose focus
     makeCode();
-}).
-on("keydown", function (e) {
+}).on("keydown", function (e) {
     if (e.keyCode == 13) {
         makeCode();
     }
