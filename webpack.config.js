29b102da403e90a7489d816a10d7a7cd8a2b0c44
OceanApart <19257940+oceanapart@users.noreply.github.com>
2024-03-04 20:02:52 +0800

    wip: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         popup: "./popup/show_qrcode.js"
     },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
     output: {
-        path: path.resolve(__dirname, "addon"),
-        filename: "[name]/show_qrcode.js"
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+        clean: true,
     },
     mode: 'none',
 };
